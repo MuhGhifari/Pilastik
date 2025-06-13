6868d124240cf56dc47d10843b422034f6fb0263
MuhGhifari <muhammadghifari37@gmail.com>
2025-06-13 23:13:57 +0700

    add tailwind and custom colors

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+     server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'aris-mac.local', // e.g. '192.168.1.10'
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
