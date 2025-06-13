cca6db25a8453e0494142dc96f437ae910db51ac
MuhGhifari <muhammadghifari37@gmail.com>
2025-06-13 18:41:22 +0700

    add first routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    Route::get('/dashboard', 'AdminController@dashboardPage')->name('admin.dashboard');
+    Route::get('/users', 'AdminController@usersPage')->name('admin.users');
+    Route::get('/trash-bins', 'AdminController@trashBinsPage')->name('admin.trash_bins');
+    Route::get('/vehicles', 'AdminController@vehiclesPage')->name('admin.vehicles');
+    Route::get('/schedules', 'AdminController@schedulesPage')->name('admin.schedules');
+});
+
+Route::prefix('collector')->group(function () {
+    Route::get('/', function () {
+        return view('collector.home');
+    })->name('collector.home');
+});
+
+Route::prefix('resident')->group(function () {
+    Route::get('/', function () {
+        return view('resident.home');
+    })->name('resident.home');
 });
